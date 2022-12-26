702fa3698c41f1e6445ec0b3e832f4a514c10b7d
MachalkaMSI <al1999dk@gmail.com>
2022-12-27 00:01:25 +0300

    classify image in for loop

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import cv2
 from torch import nn
 import time
 import os
+import random
 
 def preprocess_image(image):
 	image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -31,31 +32,35 @@ MODELS = {
 
 models_keys = list(MODELS.keys())
 
-print("[INFO] loading {}...".format(models_keys[0]))
+print_depth = 0
+
+print(f"{'\t'*print_depth}[INFO] loading ImageNet labels...")
+imagenetLabels = dict(enumerate(open(config.IN_LABELS)))
+print(f"{'\t'*print_depth}[INFO] loading {models_keys[0]}...")
 model = MODELS[models_keys[0]].to(config.DEVICE)
 model.eval()
+print_depth +=1
+for current_image in random.choices(images_names, k=10):
+	print(f"{'\t'*print_depth}[INFO] loading image:", current_image)
+	image = cv2.imread(current_image)
+	orig = image.copy()
+	image = preprocess_image(image)
+	image = torch.from_numpy(image)
+	image = image.to(config.DEVICE)
 
-print("[INFO] loading image:", images_names[0])
-image = cv2.imread(images_names[0])
-orig = image.copy()
-image = preprocess_image(image)
-image = torch.from_numpy(image)
-image = image.to(config.DEVICE)
-print("[INFO] loading ImageNet labels...")
-imagenetLabels = dict(enumerate(open(config.IN_LABELS)))
-
-print("[INFO] classifying image with '{}'...".format(models_keys[0]))
-logits = model(image)
-probabilities = nn.Softmax(dim=-1)(logits)
-sortedProba = torch.argsort(probabilities, dim=-1, descending=True)
-for (i, idx) in enumerate(sortedProba[0, :5]):
-	print("{}. {}: {:.2f}%".format
-		(i, imagenetLabels[int(idx.item())].strip(),
-		probabilities[0, idx.item()] * 100))
-
-(label, prob) = (imagenetLabels[probabilities.argmax().item()],
-	probabilities.max().item())
-cv2.putText(orig, "Label: {}, {:.2f}%".format(label.strip(), prob * 100),
-	(10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
-cv2.imshow("Classification", orig)
-cv2.waitKey(0)
+	print(f"{'\t'*print_depth}[INFO] classifying image with '{models_keys[0]}'...")
+	logits = model(image)
+	probabilities = nn.Softmax(dim=-1)(logits)
+	sortedProba = torch.argsort(probabilities, dim=-1, descending=True)
+	print(f"{'\t'*print_depth}Image {current_image} result")
+	print_depth +=1
+	for (i, idx) in enumerate(sortedProba[0, :5]):
+		print(f"{'\t'*print_depth}{i}. {imagenetLabels[int(idx.item())].strip()}: {probabilities[0, idx.item()] * 100}")
+	print_depth -=1
+	(label, prob) = (imagenetLabels[probabilities.argmax().item()],
+		probabilities.max().item())
+	# cv2.putText(orig, "Label: {}, {:.2f}%".format(label.strip(), prob * 100),
+	# 	(10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
+	# cv2.imshow("Classification", orig)
+	# cv2.waitKey(0)
+print_depth -=1
